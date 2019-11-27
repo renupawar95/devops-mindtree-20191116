6c6b02476fee8d4aff2e530847fd389b97589c45
root <root@ip-172-31-4-185.ec2.internal>
2019-11-27 07:31:29 +0000

    myfilechange

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,9 @@
 variable "key_name" {
-  default = "amit-personal-aws"
+  default = "newec"
 }
 
 variable "pvt_key" {
-  default = "/root/.ssh/amit-personal-aws.pem"
+  default = "/root/.ssh/newec.pem"
 }
 
 variable "us-east-zones" {
@@ -11,5 +11,5 @@ variable "us-east-zones" {
 }
 
 variable "sg-id" {
-  default = "sg-0a6e342b795ebb4b8"
+  default = "sg-04c5365e7f62b736e"
 }
